8052494bb3d5a068e5acf2b991003d4a19dac72a
Alexander Hyriavets <alexhyriavets@gmail.com>
2020-04-26 17:15:15 +0300

    change redirect url

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientId = '6d14161f0e268ed7c239';
-const redirectUrl = '/auth';
+const redirectUrl = 'https://ghrepos-app.herokuapp.com/auth';
 
 export const setAuthorizationHeader = (accessToken: string | null) => {
   if (accessToken) {
